719358902fe95f0e16d195588a2398ada4ab7399
Paul Jolly <paul@myitcv.io>
2018-05-03 10:05:09 +0100

    Repin against myitcv.io/hybridimporter (#63)

--- _scripts/installGo.sh
+++ _scripts/installGo.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-set -eu
+source "${BASH_SOURCE%/*}/common.bash"
 
 target=$HOME/gotip
 
-echo "Will install ${GOTIP_VERSION:0:10} to $target"
+echo "Will install ${GOTIP_VERSION:0:10} from $GOTIP_REPO to $target"
 
 mkdir -p $target
 cd $target
+
 if [ ! -e .git ]
 then
-	git clone -q https://github.com/golang/go .
+	git clone -q $GOTIP_REPO .
+else
+	git fetch -q $GOTIP_REPO $GOTIP_VERSION
 fi
 
 git checkout -qf $GOTIP_VERSION
